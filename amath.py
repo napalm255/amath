bed94ea154beb1289eff7304c5536ede363ef7f9
napalm255 <napalm255@gmail.com>
2016-02-28 02:38:13 -0500

    pylint clean up

--- amath.py
+++ amath.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
+"""
+amath is an advanced math module
+"""
 
 import math
 
 def is_even(num):
+    """returns true if given number is even"""
     # if num is evenly divisible by 2 then it's even
     return num % 2 == 0
 
 def is_odd(num):
+    """returns true if given number is odd"""
     # if num is not evenly divisible by 2 then it's odd
     return num % 2 != 0
 
 def is_perfect(num):
+    """returns true if given number is perfect"""
     # initialize the sum
     s = 0
     # loop through all numbers from 1 to num
     for i in range(1, num):
         # if number (i) is a proper positive divisor to num then
         # add the number (i) to the sum
-        if(num % i == 0):
+        if num % i == 0:
             s += i
     # if the sum matches num then it is a perfect number
-    if (s == num):
+    if s == num:
         return True
     return False
 
 def is_prime(num):
+    """return true if given number is prime"""
     # no number less than 2 is prime
-    if num < 2: return False
+    if num < 2:
+        return False
     # check if num is divisible by any number up to the square root of num
     for i in range(2, int(math.sqrt(num)) + 1):
         # if num is evenly divisible by number (i) then
@@ -36,12 +44,14 @@ def is_prime(num):
     return True
 
 def is_palindrome(num):
+    """return true if given number is a palindrome"""
     # check if num equals the reversal of num
     if str(num) == str(num)[::-1]:
         return True
     return False
 
 def get_facts(num):
+    """return dict with facts of given number"""
     f = {
         'palindrome': is_palindrome(num),
         'perfect'   : is_perfect(num),
@@ -52,10 +62,13 @@ def get_facts(num):
     return f
 
 def facts(*args):
-    # int = single integer to gather facts = facts(6)
-    # list = list of numbers to gather facts = facts([6,28])
-    # int,int = two integers defining start an and end range to gather facts = facts(1,33)
-    if len(args) > 2: return False
+    """return dict with facts for given number(s)
+       int = single integer to gather facts = facts(6)
+       list = list of numbers to gather facts = facts([6,28])
+       int,int = two integers defining start an and end range to gather facts = facts(1,33)
+    """
+    if len(args) > 2:
+        return False
     elif len(args) == 1:
         if isinstance(args[0], int):
             return get_facts(args[0])
@@ -66,8 +79,8 @@ def facts(*args):
             return f
     elif len(args) == 2:
         if isinstance(args[0], int) and isinstance(args[1], int):
-            f = {} 
-            for i in range(args[0],args[1] + 1):
+            f = {}
+            for i in range(args[0], args[1] + 1):
                 f[i] = get_facts(i)
             return f
     return
--- run.py
+++ run.py
@@ -4,9 +4,9 @@ import amath
 
 a = amath.facts(6)
 print(a)
-a = amath.facts([6,28,496])
+a = amath.facts([6, 28, 496])
 print(a)
-a = amath.facts(1,33)
+a = amath.facts(1, 33)
 print(a)
-a = amath.facts(1,33,44)
+a = amath.facts(1, 33, 44)
 print(a)
