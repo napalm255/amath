7b6556daa7850ec89760cc53369a1086e25ce2a4
napalm255 <napalm255@gmail.com>
2016-02-29 22:55:22 -0500

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 pn=(6 28 496 8128 33550336 8589869056 137438691328 2305843008139952128 2658455991569831744654692615953842176 191561942608236107294793378084303638130997321548169216)
 
-pn=(8128 8589869056 137438691328 2305843008139952128)
+pn=(137438691328 2305843008139952128)
 
 echo ":: v2:"
 for i in "${pn[@]}"
 do
-  time python isperfect_v2.py ${i}
+  echo ${i}
+  time python a.py v2 ${i}
   echo "------------"
 done
 
-echo ":: v1:"
+echo ":: v3:"
 for i in "${pn[@]}"
 do
-  time python isperfect.py ${i}
+  echo ${i}
+  time python a.py v3 ${i}
+  echo "------------"
+done
+
+echo ":: v4:"
+for i in "${pn[@]}"
+do
+  echo ${i}
+  time python a.py v4 ${i}
   echo "------------"
 done
