98b923f6e50925f58102d5790a9bcddef6decaa9
napalm255 <napalm255@gmail.com>
2016-03-01 14:57:58 -0500

    misc

--- isperfect.py
+++ isperfect.py
@@ -5,7 +5,7 @@ import amath
 if len(argv) > 1:
     if argv[1].isdigit():
         num = int(argv[1])
-        print(amath.is_perfect_list(num))
+        print(amath.is_perfect(num))
     else:
         print('must enter a digit')
 else:
