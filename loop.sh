06e16a94dffab47d06bc1b78ea99a45cd50ad237
napalm255 <napalm255@gmail.com>
2016-03-01 15:40:38 -0500

    updated loop

--- loop.sh
+++ loop.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 pn=(6 28 496 8128 33550336 8589869056 137438691328 2305843008139952128 2658455991569831744654692615953842176 191561942608236107294793378084303638130997321548169216)
 
-if [ -e "loop.last" ]
-then
+if [ -e "loop.last" ]; then
     i=`cat loop.last`
 else
     i=0
@@ -11,9 +10,11 @@ fi
 echo "starting test with ${i}"
 while true
 do
-  if [ "$(python isperfect.py ${i})" == "True" ]
-  then
+  if [ "$(python isperfect.py ${i})" == "True" ]; then
     echo ${i} >> loop.perf
+    if [ -e "sms.sh" ]; then
+      echo "perfect number: ${i}" | sms.sh
+    fi
     echo ${i}
   fi
   echo ${i} > loop.last
